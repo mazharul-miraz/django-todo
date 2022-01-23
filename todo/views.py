2329a1c3276db587fd2857d6114ed562f79393fc
mazharul-miraz <mazharul.miraz@outlook.com>
2022-01-23 23:47:22 +0900

    this is first commit

---
+++ todo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def createnote(request):
+    return render(request,"index.html")
+
+
+
+def viewdata(request):
+    return render(request,"view.html")
